b911e39c3036077b2b086409f2c14b773b6ffb24
Aneeq-Tahir <aneeqtaslam391@gmail.com>
2023-09-13 15:36:25 +0500

    first commit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -27,7 +27,7 @@ const TodoList = async () => {
             >
                <div className="flex items-center gap-2">
                   <div className="w-2 h-2 rounded-full bg-blue-600"></div>
-                  <p>{e.task}</p>
+                  <p className="w-48 whitespace-initial">{e.task}</p>
                </div>
                {data.length > 3 && <DeleteBtn id={e.id} />}
             </div>
